9e460bca0cc85ce5538e14e3fb8487e609cbdcbe
Rati <rziraqashvili@gmail.com>
2025-11-30 16:22:00 +0400

    configured auth schema in prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import db from "./prisma";
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
+    database: prismaAdapter(prisma, {
         provider: "postgresql"
-    })
-})
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
+});
